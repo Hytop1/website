2141c141afd1e16a8a4edfb3d6ccd7a632b296a7
Hytop1 <158772729+Hytop1@users.noreply.github.com>
2024-02-03 20:19:10 +0100

    Add files via upload

---
+++ WEB/front/font.js
@@ -0,0 +1,11 @@
+/* font-family: 'Kaushan Script', cursive; */
+/* font-family: 'Pinyon Script', cursive; */
+/* font-family: 'Petit Formal Script', cursive; */
+/* font-family: 'Aguafina Script', cursive; */
+/* font-family: 'Rouge Script', cursive; */
+// font-family: 'Roboto', sans-serif;
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
